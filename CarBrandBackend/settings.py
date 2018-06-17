f40908fa1012e8da69a2bcd420038f4ae578d361
yanhongchang <yanhongchang5@163.com>
2018-06-17 20:44:47 +0800

    sqlite --> mysql

--- CarBrandBackend/settings.py
+++ CarBrandBackend/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = 'CarBrandBackend.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carbrand',
+        'USER': 'root',
+        'PASSWORD': 'passw0rd',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
