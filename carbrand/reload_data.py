fb2906d73bf3c409738952c30b525e20595719e8
yanhongchang <yanhongchang5@163.com>
2018-07-15 21:13:53 +0800

    add introduction and update reload_data

--- carbrand/reload_data.py
+++ carbrand/reload_data.py
@@ -9,21 +9,27 @@ import MySQLdb
 def save_carbrand_data():
     db = MySQLdb.connect("localhost", "root", "passw0rd", "carbrand", charset='utf8')
     cursor = db.cursor()
-    with open('../car_new.json', 'r') as file:
+    with open('../car_new_01.json', 'r') as file:
         list_car = json.load(file)
         for car in list_car:
-            print car['name_cn']
+            #print car['name_cn']
+            #print car
             #print type(str(list_car[0]['name_cn']))
             name_cn = car['name_cn'].encode('utf-8')
             img = car['img'].encode('utf-8')
             carbrand_id = car['carbrand_id']
+            introduction = car['introduction'].encode('utf-8')
+            if "'" in introduction:
+                print introduction
+                intr = introduction.replace("'", "\\\'")
             ranking = car['ranking']
             if len(str(carbrand_id)) == 6:
                 country_id = int(str(carbrand_id)[0:4])
             else:
                 country_id = int(str(carbrand_id)[0:5])
-            sql = "INSERT INTO carbrand_carbrand(NAME_CN, IMG, CARBRAND_ID, RANKING, COUNTRY_ID) VALUES ('%s','%s','%d','%d','%d') " \
-                  % (name_cn, img, carbrand_id, ranking, country_id)
+            sql = "INSERT INTO carbrand_carbrand(NAME_CN, IMG, CARBRAND_ID, " \
+                  "RANKING, COUNTRY_ID, INTRODUCTION) VALUES ('%s','%s','%d','%d','%d','%s') " \
+                  % (name_cn, img, carbrand_id, ranking, country_id, intr)
             print sql
             try:
                 cursor.execute(sql)
